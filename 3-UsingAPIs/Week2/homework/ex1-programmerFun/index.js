f02f080fb76bb09ddb0a7e6bd87549f80f4dc87f
lynn@aspire1 <lynn@aspire1>
2022-03-28 13:51:58 +0200

    ex1 passed

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -18,28 +18,36 @@ Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-Usin
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
 function requestData(url) {
-  // TODO return a promise using `fetch()`
+ return fetch(url)
+ .then(response => response.json())
+ .catch((error) => {
+   console.log(`Oops.. something went wrong! ${error}`);
+  });
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
-  console.log(data);
+  const img = document.createElement('img');
+  img.src = data.img;
+  img.alt = data.title;
+  document.body.appendChild(img);
+  console.log(img);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
-  console.log(error);
+  const errMsg = document.createElement('h1');
+  errMsg.textContent = `Oops.. something went wrong! ${error}`;
+  document.body.appendChild(errMsg);
+  console.log(errMsg);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
+async function main() {
+  try{
+  const jsonResponse = await requestData('https://xkcd.now.sh/?comic=latest');
+  renderImage(jsonResponse);
+  }
+    catch (error) {
       renderError(error);
-    });
+  }
 }
 
 window.addEventListener('load', main);
